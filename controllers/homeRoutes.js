9f63e381e92c33cf4edb2cf5646c9b7176c2e696
Kianna Reid <kiannareid@yahoo.com>
2022-10-13 18:05:33 -0400

    undo

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,114 @@
+const router = require("express").Router();
+const { Blog, User } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    // Get all blogs and JOIN with user data
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const blog = blogData.map((blog) => blog.get({ plain: true }));
+    console.log(blog);
+    // Pass serialized data and session flag into template
+    res.render("homepage", {
+      blog,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/blog/:id", async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    const blog = blogData.get({ plain: true });
+
+    res.render("blog", {
+      ...blog,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get("/dashboard", withAuth, async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+      where: { user_id: req.session.user_id },
+    });
+
+    // Serialize data so the template can read it
+    const blog = blogData.map((blog) => blog.get({ plain: true }));
+    // Find the logged in user based on the session ID
+
+    res.render("dashboard", {
+      blogs: blog,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/dashboard/:id", withAuth, async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const blogData = await Blog.findOne({
+      include: [
+        {
+          model: User,
+        },
+      ],
+      where: { blog_id: req.params.id },
+    });
+
+    // Serialize data so the template can read it
+    const blog = blogData.get({ plain: true });
+    // Find the logged in user based on the session ID
+
+    res.render("dashboard", {
+      blogs: blog,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
